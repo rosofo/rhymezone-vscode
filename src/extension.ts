3cc9447e1667047684f483e3a6e34d7385ae30db
Robin Forbes <rsforbes0@gmail.com>
2024-01-20 14:17:34 +0000

    remove completion & synonyms from hover

--- src/extension.ts
+++ src/extension.ts
@@ -53,63 +53,15 @@ export function activate(context: vscode.ExtensionContext) {
     });
   });
 
-  const completionProvider = vscode.languages.registerCompletionItemProvider(
-    "markdown",
-    {
-      async provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        const wordRange = document.getWordRangeAtPosition(
-          position,
-          /[\w\-\]]+/
-        );
-        let word = document.getText(wordRange);
-        word = word.slice(0, word.length - 1);
-
-        console.log("word:", word);
-        console.log("wordRange:", wordRange);
-
-        let rhymes = await getCachedRhymes(word);
-        if (!rhymes) {
-          rhymes = await fetchRhymes(word);
-          await setCachedRhymes(word, rhymes);
-        }
-        console.log("rhymes:", rhymes);
-
-        return rhymes.slice(0, 20).map((rhyme) =>
-          // a simple completion item which inserts `Hello World!`
-          {
-            /* a simple completion item which inserts `Hello World!`*/
-            const completion = new vscode.CompletionItem({ label: rhyme });
-            completion.range = wordRange;
-            completion.filterText = word;
-            return completion;
-          }
-        );
-      },
-    },
-    "]"
-  );
-
   const hoverProvider = vscode.languages.registerHoverProvider("markdown", {
     async provideHover(document, position, token) {
       const word = document.getText(document.getWordRangeAtPosition(position));
       const definition = await fetchCachedDefinition(word);
-      const synonyms = await fetchCachedSynonyms(word);
 
       const markdown = new vscode.MarkdownString();
       markdown.isTrusted = true;
       markdown.supportHtml = true;
       markdown.appendMarkdown(`### ${word}\n`);
-      for (const synonym of synonyms) {
-        const command = `command:rhymezone.replace?${encodeURIComponent(
-          JSON.stringify([position, synonym])
-        )}`;
-        markdown.appendMarkdown(`- [${synonym}](${command})\n`);
-      }
       markdown.appendMarkdown(definition);
       return new vscode.Hover(markdown);
     },
@@ -129,5 +81,5 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(completionProvider, replaceCommand, hoverProvider);
+  context.subscriptions.push(replaceCommand, hoverProvider);
 }
